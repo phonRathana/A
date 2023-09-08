ee66b726219ae2847eba8c6d4fc98a90083f7112
Phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-09-08 23:20:58 +0700

    Project.js

---
+++ Project.js
@@ -0,0 +1,18 @@
+//Implement1
+
+<script>
+gtag('set', 'user_data', {
+     "email": yourEmailVariable, ***Change yourEmailVariable to the actual JavaScript variable name where you are storing the user’s email data. Do the same                     for the other variables below. Make sure the values aren't hashed.
+      "phone_number": yourPhoneVariable,
+      "address": {
+      "first_name": yourFirstNameVariable,
+      "last_name": yourLastNameVariable,
+      "street": yourStreetAddressVariable,
+      "city":yourCityVariable,
+      "region": yourRegionVariable,
+      "postal_code": yourPostalCodeVariable,
+      "country": yourCountryVariable
+    }
+  });
+</script>
+        
